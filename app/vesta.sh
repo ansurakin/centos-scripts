a7f0658f79a42a8692c227d69c150f055a85149c
alex <alex@example.com>
2020-03-11 01:49:16 +0300

    рефакторинг

--- app/vesta.sh
+++ app/vesta.sh
@@ -6,8 +6,9 @@ cd /opt
 curl -O http://vestacp.com/pub/vst-install.sh
 bash vst-install.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim no --dovecot no --spamassassin no --clamav no --softaculous no --mysql no --postgresql no --hostname pgmaster.ru --email sanringo@mail.ru --password 12345678
 
+#TODO доработать. Тут не выполняется
 export VESTA=/usr/local/vesta/
-sed -i -e '$a\DB_SYSTEM=\x27mysql,pgsql\x27' $VESTA/conf/vesta.conf
+sed -i -e '$a\DB_SYSTEM=\x27mysql,pgsql\x27' $VESTA/conf/vesta.conf#TODO дублирует?
 sed -i "s|service='postgresql'|service='postgresql-9.6'|g" $VESTA/bin/v-list-sys-services
 #Регистрируем базу в панели
 $VESTA/bin/v-add-database-host pgsql 127.0.0.1 postgres 12345678
