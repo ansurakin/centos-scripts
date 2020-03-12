683322fa22981d5ec4e5acc6742f19627dc0d233
alex <alex@example.com>
2020-03-13 01:52:18 +0300

    рефакторинг

--- app/vesta.sh
+++ app/vesta.sh
@@ -8,11 +8,11 @@ bash vst-install.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes 
 
 #TODO доработать. Тут не выполняется
 echo "test111"
-export VESTA=/usr/local/vesta/
+export VESTA=/usr/local/vesta
 sed -i -e '$a\DB_SYSTEM=\x27mysql,pgsql\x27' $VESTA/conf/vesta.conf#TODO дублирует?
 sed -i "s|service='postgresql'|service='postgresql-9.6'|g" $VESTA/bin/v-list-sys-services
 #Регистрируем базу в панели
-$VESTA/bin/v-add-database-host pgsql 127.0.0.1 postgres 12345678
+$VESTA/bin/v-add-database-host pgsql 127.0.0.1 postgres 12345678#Нужно ли тут?
 #service apache2 restart
 
 echo "vesta end"
